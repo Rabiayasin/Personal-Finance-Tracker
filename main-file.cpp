e77c73fac2514c421290a114ed33608965be7f30
Rabiayasin <spacewalker1085@gmail.com>
2024-12-13 22:36:49 +0500

    main file to write all the code

---
+++ main-file.cpp
@@ -0,0 +1,15 @@
+// Including all the Header files
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+// All the functions:
+
+
+
+int main(){
+
+    return 0;
+}

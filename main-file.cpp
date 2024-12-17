4512d7996d1874f4571f729355e1dc9c7dc27ac2
Rabiayasin <spacewalker1085@gmail.com>
2024-12-17 09:52:42 +0500

    updated code

--- main-file.cpp
+++ main-file.cpp
@@ -12,6 +12,9 @@ int MAX_ENTRIES = 100;
 
 void addEntry(string type, float amount, string category, string date);
 void diplayMenu();
+void askUser();
+void saveDataToFile(const string& filename, float amounts[][2], string categories, string dates[], int currentIndex);
+void calculateBalance(float amounts[][2], int currentIndex, float &totalIncome, float &totalExpenses, float &balance);
 
 
 int main(){
@@ -26,16 +29,42 @@ void addEntry(bool type, float amount, string category, string date,
     2D array to store amount and its type (0 for expense and 1 for income) 
     1D array to store category and date*/
 
-    // convert bool type into float
-    float type = (type == true) ? 1 : 0;
+    // Check if there's space for more enteries
+    if (currentIndex >= MAX_ENTRIES) {
+        cout << "No more space for entries." << endl;
+    }
 
     // store the data in the arrays
-    float amounts[MAX_ENTRIES][2] // amounts and their type
-    string categories[MAX_ENTRIES] = category;
-    string dates[MAX_ENTRIES] = date;
+    amounts[currentIndex][0] = amount;
+    amounts[currentIndex][1] = (type == true) ? 1 : 0; // converting bool to float
+    categories[currentIndex] = category;
+    dates[currentIndex] = date;
 
-
-
-
-
+    currentIndex++;
+    cout<<"Entry added successfully!"<<endl;
 }
+
+
+void saveDataToFile(const string& file, float amounts[][2], string categories, string dates[], int currentIndex){
+    // open file for writing
+    ofstream outFile(file);
+
+    // check if file opened successfully 
+    if (!outFile){
+        cout<<"Error: COuld not open file for writing."<<endl;
+        return;
+    }
+
+    // Write each entry into the file
+    for (int i=0; i<currentIndex, i++){
+        outFile << amounts[i][0] << ","
+                << amounts[i][1] << ","
+                << categories[i] << ","
+                << dates[i] << endl;
+    }
+
+    // close the file
+    outFile.close();
+    cout << "Data saved successfully to file: " << file <<endl;
+}
+

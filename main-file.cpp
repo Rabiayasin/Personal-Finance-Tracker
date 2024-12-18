9ffe42390e9c63e8221ab44257d360eeb65162b4
ridafaakhar1 <ridafaakhar@gmail.com>
2024-12-18 20:53:43 +0500

    Updated code

--- main-file.cpp
+++ main-file.cpp
@@ -6,20 +6,10 @@
 #include <fstream>
 using namespace std;
 
-<<<<<<< HEAD
-
-// \hey I'm Rida
-=======
-<<<<<<< HEAD
-// \hey I'm Rida
-=======
->>>>>>> d6bff0a09c725d2bdc1259bc70d715863b1ad2c8
->>>>>>> Rida-Syed
-
 // Global variables or constants
 int MAX_ENTRIES = 100;
 const int MAX_MONTHS = 12;
-
+ 
 // const int ALL_EXPENSES =  100;
 // int expenseCount = 0;
 // string category[ALL_EXPENSES];
@@ -80,13 +70,15 @@ int main() {
     	  cout<<"Enter the income for month: "<<i+1;
     	    cin>>income[i];
     	    cout<<endl;
-	} 
+	} //loop for entering income for each month
     int numMonths = 12; 
     cout<<"Enter the month for which you want to display the total savings: ";
     cin>>targetMonth;
     double monthlySavings = calculateMonthlySavings(months, income, expenses, numMonths, targetMonth);
-    cout << "Savings for " << targetMonth << ": " << monthlySavings << endl;;
+    //function for calculating monthly savings
+    cout << "Savings for " << targetMonth << ": " << monthlySavings << endl; 
     double totalSavings = calculateTotalSavings(income, expenses, numMonths);
+    //function for calculating total savings
     cout << "Total Savings: " << totalSavings << endl;
              break;
             }
@@ -181,7 +173,7 @@ void saveDataToFile(const string& file, float amounts[][2], string categories, s
 
 
 
-double calculateMonthlySavings(const string months[], const double income[], const double expenses[], int numMonths, const std::string& targetMonth) {
+double calculateMonthlySavings(const string months[], const double income[], const double expenses[], int numMonths, const string &targetMonth) {
     for (int i = 0; i < numMonths; ++i) {
         if (months[i] == targetMonth) {
             return income[i] - expenses[i];
@@ -190,15 +182,6 @@ double calculateMonthlySavings(const string months[], const double income[], con
      cout << "Error: Month not found.\n";
     return 0.0;
 }
-    for (int i = 0; i < numMonths; ++i) {
-        if (months[i] == targetMonth) {
-            return income[i] - expenses[i];
-        }
-    }
-     cout << "Error: Month not found.\n";
-    return 0.0;
-}
-
 
 double calculateTotalSavings(const double income[], const double expenses[], int numMonths) {
     double totalSavings = 0.0;
@@ -206,4 +189,20 @@ double calculateTotalSavings(const double income[], const double expenses[], int
         totalSavings += (income[i] - expenses[i]);
     }
     return totalSavings;
+}
+
+void checkExpensesByDate(const string dates[], const double amount[], int size, const string &targetDate){
+    bool found= false;
+
+    cout<<"Expense for date: "<<targetDate<<endl;
+
+    for(int i=0; i<size; i++){
+        if(dates[i]==targetDate){
+            cout<<"Amounts: $ "<<amount[i]<<endl;
+            found=true;
+        }
+    }
+    if(!found){
+        cout<<"No expenses for given date! "<<endl;
+    }
 }
